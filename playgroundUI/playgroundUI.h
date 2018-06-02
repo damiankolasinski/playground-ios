a892b1bc20dcf66f9d0f296bb7e717cfc3625861
Damian Kolasinski <damian.kolasinski@appunite.com>
2018-06-02 23:50:23 +0200

    added ui framework

---
+++ playgroundUI/playgroundUI.h
@@ -0,0 +1,19 @@
+//
+//  playgroundUI.h
+//  playgroundUI
+//
+//  Created by Damian Kolasiński on 02/06/2018.
+//  Copyright © 2018 Damian Kolasiński. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for playgroundUI.
+FOUNDATION_EXPORT double playgroundUIVersionNumber;
+
+//! Project version string for playgroundUI.
+FOUNDATION_EXPORT const unsigned char playgroundUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <playgroundUI/PublicHeader.h>
+
+
